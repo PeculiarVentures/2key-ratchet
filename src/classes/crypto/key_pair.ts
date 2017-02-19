b4fbb8a91940cc0aa715c81b0317d9fc32d89ffb
microshine <microshine@mail.ru>
2017-02-19 12:41:48 +0300

    Add comment

--- src/classes/crypto/key_pair.ts
+++ src/classes/crypto/key_pair.ts
@@ -1,3 +1,12 @@
+/**
+ * 
+ * 2key-ratchet
+ * Copyright (c) 2016 Peculiar Ventures, Inc
+ * Based on https://whispersystems.org/docs/specifications/doubleratchet/ and 
+ * https://whispersystems.org/docs/specifications/x3dh/ by Open Whisper Systems
+ * 
+ */
+
 import { ECPublicKey } from "./public_key";
 
 export interface ECKeyPair {
